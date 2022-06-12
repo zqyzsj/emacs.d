d995be2e7f870001e88af72fde2728cb9166740b
shijun zhao <zqyzsj@gmail.com>
2022-06-12 15:25:04 +0800

    rust-analyzer: add rust-analyzer
    add rust-analyzer for rust programming. Only add lsp-mode.
    reference:
    https://robert.kra.hn/posts/rust-emacs-setup/
    
    Signed-off-by: <zqyzsj@gmail.com>

---
+++ lisp/init-rust-analyzer.el
@@ -0,0 +1,24 @@
+(use-package lsp-mode
+  :ensure t
+  :hook (rust-mode . lsp)
+  :commands lsp
+  :custom
+  ;; what to use when checking on-save. "check" is default, I prefer clippy
+  (lsp-rust-analyzer-cargo-watch-command "clippy")
+  (lsp-eldoc-render-all t)
+  (lsp-idle-delay 0.6)
+  ;; enable / disable the hints as you prefer:
+  (lsp-rust-analyzer-server-display-inlay-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  (lsp-rust-analyzer-display-chaining-hints t)
+  (lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  (lsp-rust-analyzer-display-closure-return-type-hints t)
+  (lsp-rust-analyzer-display-parameter-hints nil)
+  (lsp-rust-analyzer-display-reborrow-hints nil)
+  :config)
+
+(use-package yasnippet
+  :ensure t
+  :hook ((lsp-mode . yas-minor-mode)))
+
+(provide 'init-rust-analyzer)
