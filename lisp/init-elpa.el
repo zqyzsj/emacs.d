f6d8930581f666d444a30c8a0162e4df151c31ba
shijun zhao <zqyzsj@gmail.com>
2022-06-14 21:47:59 +0800

    mirror: use tsinghua elpa mirror
    change my mirror to be tsinghua elpa mirror, as the ustc mirror CANNOT
    auto download s.el correctly. The ustc mirror usually downloads a null
    s.el package.
    
    s.el: The long lost Emacs string manipulation library.
    
    Signed-off-by: <zqyzsj@gmail.com>

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -16,9 +16,8 @@
 ;;; Standard package repositories
 
 ;; (add-to-list 'package-archives '( "melpa" . "https://melpa.org/packages/") t)
-(setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
-                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
-                         ("nongnu" . "http://mirrors.ustc.edu.cn/elpa/nongnu/")))
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 ;; Official MELPA Mirror, in case necessary.
 ;;(add-to-list 'package-archives (cons "melpa-mirror" (concat proto "://www.mirrorservice.org/sites/melpa.org/packages/")) t)
 
